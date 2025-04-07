24f6d7bd6303e174f03b9ddb21c6e74f173b8e7b
Thiago Assis <thiago.medassis@gmail.com>
2025-04-07 17:25:02 -0300

    Init project

---
+++ app/core/models.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
+from django.db import models
+
+
+class User(AbstractUser):
+
+    # Fields
+    cpf = models.CharField(
+        "cpf",
+        max_length=11,
+        unique=True,
+        validators=[MinLengthValidator(11)],
+        blank=True,
+        null=True,
+        default=None,
+    )
+    phone = models.CharField(
+        "telefone",
+        max_length=11,
+        unique=True,
+        validators=[MinLengthValidator(11)],
+        blank=True,
+        null=True,
+        default=None,
+    )
+    birth_date = models.DateField(
+        "data de nascimento",
+        blank=True,
+        null=True,
+        default=None,
+    )
+
+    # Relationships
+
+    # Settings
+    USERNAME_FIELD = "username"
+
+    # Functions
+    def __str__(self):
+        return self.get_full_name()
+
+    def full_name(self):
+        return self.__str__()
+
+    full_name.short_description = "nome"
+
+    class Meta:
+        db_table = "core_user"
+        verbose_name = "usuário"
+        verbose_name_plural = "usuários"
